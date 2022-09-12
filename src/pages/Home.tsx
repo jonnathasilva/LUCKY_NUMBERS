e7238676cdabe758869b2e81a1f9ecb330221933
jonnathan silva <sjonnathan928@gmail.com>
2022-09-12 14:27:53 -0300

    change color

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext, ChangeEvent, useEffect } from "react";
+import { useContext, ChangeEvent, useEffect, useState } from "react";
 import { Context } from "../context/APIContext";
 import LogoSena from "../imgs/Logo_Sena.png";
 import "./Home.css";
@@ -8,7 +8,13 @@ interface LoteriasConcursos {
   loteriaId: number;
 }
 
+interface Loterias {
+  nome: string;
+  id: number;
+}
+
 function Home() {
+  const [MyLoteria, setMyLoteria] = useState<Loterias>({} as Loterias);
   const { loteria, GetLoteriasById, concurso, loteriasConcursos } =
     useContext(Context);
 
@@ -18,9 +24,26 @@ function Home() {
     GetLoteriasById(_id as LoteriasConcursos);
   }
 
+  async function myLoteria() {
+    const filterLoteriasConcursos = loteriasConcursos.find(
+      (e) => e.concursoId === concurso.id
+    );
+
+    const filterLoteria = loteria.find(
+      (e) => e.id === filterLoteriasConcursos?.loteriaId
+    ) as Loterias;
+
+    // console.log(filterLoteriasConcursos, filterLoteria);
+    setMyLoteria(filterLoteria);
+  }
+
+  useEffect(() => {
+    myLoteria();
+  }, [concurso]);
+
   return (
     <div className="container">
-      <section className="loteria">
+      <section className={`loteria ${MyLoteria?.nome}`}>
         <select name="concursos" onChange={handleChang}>
           {loteria.map((Item) => (
             <option key={Item.id} value={Item.id}>
@@ -31,7 +54,7 @@ function Home() {
         <div>
           <img src={LogoSena} alt="Sena" />
         </div>
-        <h1>Mega-Sena</h1>
+        <h1>{MyLoteria?.nome}</h1>
         <p>Concurso Nº 4560</p>
       </section>
       <section className="luckyNumber">
